4d2380c9af8538851f817d93ec2963e49df16b03
Proatscratch <91508072+Proatscratch@users.noreply.github.com>
2022-03-28 14:33:31 -0700

    Update Physhics.py

--- PyDisEng/Physhics.py
+++ PyDisEng/Physhics.py
@@ -10,16 +10,22 @@ class Pobject:
             self.Sprite = Sprite
             self.Pos = (Sprite.x,Sprite.y)
             self.Kin = Kin
-      def loop(self,isKin=True):
+      def loop(self,isKin=True,pysobject):
             '''Loops through the pyshical object
             :isKin | defaults to True if False must be directly moved
             '''
             self.mech = self.Pos[0]+self.Kin[0],self.Kin[1]+self.Pos[1]
             self.Pos = (Sprite.x,Sprite.y)
+            p = self.Pos
             self.Pos = list(self.Pos)
             self.Pos[0] +=self.Kin[0]
             self.Pos[1] +=self.Kin[1]
+            self.Kin[0]-=1 if self.Kin[0] else 0
+            self.Kin[1]-=1 if self.Kin[0] else 0 
             self.Pos = tuple(self.Pos)
+            Sprite.x,Sprite.y = self.Pos
             check_distances = () #checks the closes distances (only if object is Kinetic)
-            if isKin:
-                  pass
+            for thing in pysobject:
+                  if self.Sprite.collide(thing):
+                        Sprite.x,Sprite.y = p
+            self.Sprite.draw()
